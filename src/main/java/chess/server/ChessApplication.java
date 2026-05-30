e589f3adb3e8ec9b1fa69b9c2529bad602fa16f4
Rishi Rajadhyaksha <rishi@Rishis-MacBook-Air-10.local>
2026-05-30 01:01:20 -0400

    Fix Spring component scanning

--- src/main/java/chess/server/ChessApplication.java
+++ src/main/java/chess/server/ChessApplication.java
@@ -3,7 +3,12 @@ package chess.server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(
+    scanBasePackages = {
+        "chess.server",
+        "chess.game"
+    }
+)
 public class ChessApplication {
     public static void main(String[] args) {
         SpringApplication.run(ChessApplication.class, args);
